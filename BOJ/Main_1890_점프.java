c3b345b0b6c1e5ff016d764b9fd3e172b5c9680c
hyejin <heo015414@gmail.com>
2023-02-24 21:34:26 +0900

    0224

---
+++ BOJ/Main_1890_점프.java
@@ -0,0 +1,38 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1890_점프 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+        long[][] dp = new long[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp[0][0] = 1;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == 0) break;
+                if (dp[i][j] != 0) {
+                    //우
+                    if (arr[i][j] + j < N) dp[i][arr[i][j] + j] += dp[i][j];
+                    //하
+                    if (arr[i][j] + i < N) dp[arr[i][j] + i][j] += dp[i][j];
+                }
+            }
+        }
+        System.out.println(dp[N - 1][N - 1]);
+    }
+}
