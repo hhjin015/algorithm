ca04e3e54ec9ab6fb41f7ca769a5f9e6aa43ad0d
hyejin <heo015414@gmail.com>
2023-03-25 21:39:14 +0900

    0325

---
+++ BOJ/Main_2468_안전영역.java
@@ -0,0 +1,61 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2468_안전영역 {
+    static int N, maxRain, max;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        maxRain = -1;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                maxRain = Math.max(maxRain, map[i][j]);
+            }
+        }
+
+        max = -1;
+        for (int rain = 0; rain <= maxRain; rain++) {
+            visited = new boolean[N][N];
+            int cnt = 0; // 안전지역
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (!visited[i][j] && map[i][j] > rain) { // 방문하지 않았거나 물에 잠기지 않는 곳
+                        cnt += dfs(i, j, rain);
+                    }
+                }
+            }
+            max = Math.max(max, cnt);
+        }
+        System.out.println(max);
+    }
+
+    private static int dfs(int r, int c, int rain) {
+        visited[r][c] = true;
+
+        for (int d = 0; d < 4; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+
+            if (nr < 0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc]) continue;
+            if (map[nr][nc] > rain) { // 물에 잠기지 않을 때
+                dfs(nr, nc, rain);
+            }
+        }
+        return 1;
+    }
+}
+
