abb06f941fe7b7fa45b3e38d25b3d4c914f85d8c
hyejin <heo015414@gmail.com>
2023-07-14 21:24:02 +0900

    0714

---
+++ BOJ/Main_1463_1로만들기.java
@@ -0,0 +1,29 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1463_1로만들기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[1000001];
+
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for (int i = 4; i <= N; i++) {
+            if (i % 6 == 0) {
+                dp[i] = Math.min(dp[i / 3], Math.min(dp[i / 2], dp[i - 1])) + 1;
+            } else if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i / 3], dp[i - 1]) + 1;
+            } else if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i / 2], dp[i - 1]) + 1;
+            } else {
+                dp[i] = dp[i - 1] + 1;
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
