d73fd4037b95cd84a67446b2d7fb000a7f66ab35
hyejin <heo015414@gmail.com>
2023-07-31 18:50:08 +0900

    0731

---
+++ BOJ/Main_1719_택배.java
@@ -0,0 +1,65 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_1719_택배 {
+    static final int INF = 199001;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[N][N];
+        String[][] path = new String[N][N];
+
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(dist[i], INF);
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken());
+
+            dist[a][b] = c;
+            dist[b][a] = c;
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i == j) continue;
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        path[i][j] = path[i][k];
+                    } else {
+                        if (k == 0) path[i][j] = Integer.toString(j + 1);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            path[i][i] = "-";
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(path[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
