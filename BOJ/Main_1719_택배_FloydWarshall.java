b18c7e8fb776472d7b8dbb19bd865750e866bfeb
hyejin <heo015414@gmail.com>
2023-08-01 19:03:00 +0900

    0731

--- BOJ/Main_1719_택배_FloydWarshall.java
+++ BOJ/Main_1719_택배_FloydWarshall.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main_1719_택배 {
+public class Main_1719_택배_FloydWarshall {
     static final int INF = 199001;
 
     public static void main(String[] args) throws IOException {
